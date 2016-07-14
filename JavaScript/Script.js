523203a17dc99dc654ff6a3f8dae35f88eeffc89
Vusumzi123 <D.O.GeeVz@gmail.com>
2016-07-13 22:43:43 -0500

    fixed BG reduced code with css
    change background sizing from javascript implementation to css vh for
    performance

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,29 +1,14 @@
 			function setVars(){
-				
-				var nodeList = document.getElementsByClassName('parCont');
-				var bgNodes = document.getElementsByClassName('bg_img');
-
 				Vars = {
-				
-					sizedElems: Array.prototype.slice.call(nodeList,0),
-
 					parallaxElems : [
-
 						document.getElementById('layer_1'),
-						document.getElementById('layer_2'),
-						document.getElementById('background')
+						document.getElementById('layer_2')
 					],
-					
-
-
-					backgrounds: Array.prototype.slice.call(bgNodes,0),
-
 					navBar : document.getElementById('navb'),
 				}
 			};
 
 			function dynamicVars(){
-
 				Dvars = {
 				hOfset : window.pageYOffset,
 				height : window.innerHeight,
@@ -32,32 +17,11 @@
 				}
 			}
 
-
-
-			function setSize(item){
-					item.style.height = Dvars.height+"px";
-			}
-
-
 			function parallax(item,ofset){
 				//var thing = item;
 				//var item = parallaxElems_1;
 				//var desp = -(window.pageYOffset/ofset);
 				item.style.top = -(Dvars.hOfset/ofset)+"px";
-			}
-			
-			function resizeBackground(item){
-				//var w = window.innerWidth;
-				//var h = window.innerHeight;
-				var back = item;
-
-				if(Dvars.widht/Dvars.height < 640/428){
-					back.style.height = "100%";
-					back.style.width = "auto";
-				}else {
-					back.style.height = "auto";
-					back.style.width = "100%";
-				}
 			}
 
 			function scrollNav(){
@@ -68,14 +32,9 @@
 				}
 			}
 
-			
-
-
-
 				function parallaxScroll(){
 					parallax(Vars.parallaxElems[0],1);
 					parallax(Vars.parallaxElems[1],23);
-					parallax(Vars.parallaxElems[2],12);
 				}
 
 				function scrl(){
@@ -86,9 +45,7 @@
 
 				function res(){
 					dynamicVars();
-					Vars.sizedElems.forEach(setSize);
 					parallaxScroll();
-					Vars.backgrounds.forEach(resizeBackground);
 				}
 
 				function set(){
@@ -96,20 +53,13 @@
 					res();
 				}
 
-
-
 			function main(){
-
 				set();
 				window.addEventListener("scroll", scrl, false);
 				window.addEventListener("resize", res, true);
-
-
 			};
-			
 
 
 			(function(){
 				window.onload = main;
 			})();
-			
