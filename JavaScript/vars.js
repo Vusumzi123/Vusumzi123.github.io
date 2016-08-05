31d507a5e0ce9fe8c3aa8febf889b7e6e6fa41e2
Vusumzi <D.O.GeeVz@gmail.com>
2016-08-04 21:31:21 -0500

    commit linux

---
+++ JavaScript/vars.js
@@ -0,0 +1,31 @@
+function setVars(){
+  vars = {
+
+    navElemes :[
+      $('#homeNv'),
+      $('#UsNv'),
+      $('#contactNv'),
+    ],
+
+    pageElems : [
+      $('#logo_header'),
+      $('#article_1'),
+      $('#beer_persicope'),
+      $('#article_2'),
+      $('#footer'),
+    ],
+
+    parallaxElems : $('#main_logo_container'),
+    navBar : $('.navbar-default'),
+    brandBtn : $('#brand_btn'),
+
+  }
+};
+
+function dynamicVars(){
+  Dvars = {
+    wHeight : $(window).height(),
+    wWidth : $(window).width(),
+    wScroll : $(window).scrollTop(),
+  }
+};
